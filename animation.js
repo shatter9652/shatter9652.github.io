402a862ee008df8172bba97b0e766e58890621c0
fanqyxl <165182838+fanqyxl@users.noreply.github.com>
2025-04-05 03:48:10 -0400

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,47 @@
+// SCRIPT SKIDDED FROM FROGGIE!!!
+
+// errr
+const textElement = document.getElementById("text");
+const overlay = document.getElementById("overlay");
+const audio = document.getElementById("audio-player");
+
+// lyrics timing n stuff
+const phrases = [
+    "",
+    "I've",
+    "I've been",
+    "I've been having",
+    "I've been having trouble",
+    "I've been having trouble sleeping..."
+];
+
+// these r in seconds btw
+const delays = [1.66, 0.25, 0.7, 0.7, 0.6, 0.6];
+
+overlay.addEventListener("click", () => {
+    textElement.textContent = "";
+    audio.play();
+});
+
+// main animation thingy 
+audio.addEventListener("play", () => {
+    let i = 0;
+    overlay.style.opacity = "1";
+    
+    function updateText() {
+        if (i < phrases.length) {
+            textElement.textContent = phrases[i];
+            setTimeout(updateText, delays[i] * 1000);
+            i++;
+        } else {
+            setTimeout(() => {
+                overlay.style.transition = "opacity 1s ease";
+                overlay.style.opacity = "0";
+                setTimeout(() => {
+                    overlay.style.display = 'none';
+                }, 1000);
+            }, 300);
+        }
+    }
+    updateText();
+});
